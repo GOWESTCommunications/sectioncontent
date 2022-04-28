0f13a8508511eb19cdfcb3c4e4525924e1f71ac6
MichaelNussbaumerGOWEST <m.nussbaumer@go-west.at>
2022-04-28 18:03:35 +0700

    fix error in new IN statements by adding zero to uids

--- Classes/Controller/TeaserController.php
+++ Classes/Controller/TeaserController.php
@@ -275,12 +275,12 @@ class TeaserController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
         );
 
         $this->allPages = [
-            'uids' => [],
+            'uids' => [0],
             'pageInfo' => [],
         ];
 
         $this->pages = [
-            'uids' => [],
+            'uids' => [0],
             'pageInfo' => [],
         ];
 
@@ -320,7 +320,7 @@ class TeaserController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
             }
             $this->pages = $this->allPages;
         }
-        $this->pages['uids'] = [];
+        $this->pages['uids'] = [0];
         foreach($this->pages['pageInfo'] as $pageInfo) {
             $this->pages['uids'][$pageInfo['uid']] = $pageInfo['uid'];
         }
@@ -383,7 +383,6 @@ class TeaserController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
     protected function addPageCategories() {
 
         $catJoinCol = ($this->sys_language_uid > 0) ? 'l10n_parent' : 'uid';
-        $this->allPages['uids'][] = 0;
         $categoryQuery = "
             SELECT
                 cmm.uid_local AS category_uid, 
