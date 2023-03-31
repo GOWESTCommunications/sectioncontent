36f8315fe0354589ab0cb55edcbe6e37a881d869
MichaelNussbaumerGOWEST <m.nussbaumer@go-west.at>
2023-03-31 13:07:43 +0700

    new version

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
         'Based on Extbase and Fluid Template Engine.',
     'category' => 'plugin',
     'shy' => 0,
-    'version' => '10.1.15',
+    'version' => '10.1.16',
     'dependencies' => '',
     'conflicts' => '',
     'priority' => '',
