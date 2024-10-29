d5818d36f43feed5a51ab20b0f0f7895cad3d04d
andigo-west <45382316+andigo-west@users.noreply.github.com>
2024-10-29 10:36:50 +0100

    add bodytext

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,16 +2,12 @@
 
 defined('TYPO3') or die();
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 (static function (): void {
 
-    
-    // \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    //     'sectioncontent',
-    //     'Pi1',
-    //     'Sectioncontent'
-    // );
-
-    $pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    $pluginSignature = ExtensionUtility::registerPlugin(
         'Sectioncontent',
         'Pi1',
         'Sectioncontent'
@@ -19,10 +15,27 @@ defined('TYPO3') or die();
 
     
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive,frame_class';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform'; 
+    
+    $GLOBALS['TCA']['tt_content']['types']['list']['columnsOverrides'] = [
+        'bodytext' => [
+            'config' => [
+                'type' => 'text',
+                'renderType' => 'text',
+                'enableRichtext' => true,  // if needed
+            ]
+        ]
+    ];
+    
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        'bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel',
+        'list',
+        'after:header'
+    );
+    
 
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    ExtensionManagementUtility::addPiFlexFormValue(
         $pluginSignature,
         'FILE:EXT:sectioncontent/Configuration/FlexForms/flexform_teaser.xml'
     );
